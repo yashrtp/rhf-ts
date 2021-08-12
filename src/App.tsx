f160bcff3f59fac86162783b691067d6f01fda4d
yash <yash.raj@truepill.com>
2021-08-12 11:13:24 +0530

    Added fields

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import './App.css';
 
+type Inputs = {
+  preferredname: string
+  emailaddress: string
+  subject: string
+  description: string
+}
+
 function App() {
+  const {register, handleSubmit, formState: {errors}} = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> =(data) => {
+    alert(JSON.stringify(data))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+       <label htmlFor="preferredname">Preferred Name</label>
+       <input id="preferredname" type="text" {...register('preferredname', {required: true, maxLength: 200})}/>
+       {
+         errors.preferredname && <div className="error"> Enter your name</div>
+       }
+      </div>
+      <div>
+       <label htmlFor="emailaddress">Email Address</label>
+       <input id="emailaddress" type="text" {...register('emailaddress' , {required: true, maxLength: 200})}/>
+       {
+         errors.emailaddress && <div className="error"> Enter your Email Address</div>
+       }
+      </div>
+      <div>
+       <label htmlFor="subject">Subject</label>
+       <input id="subject" type="text"{...register('subject', {required: true, maxLength: 200})}/>
+       {
+         errors.subject && <div className="error"> Enter the Subject</div>
+       }
+      </div>
+      <div>
+       <label htmlFor="description">Description</label>
+       <input id="description" type="text"{...register('description', {required: true, maxLength: 200})}/>
+       {
+         errors.description && <div className="error"> Enter the description </div>
+       }
+      </div>
+      <button type ="submit">Submit</button>
+
+    </form>
+    </main>
+   
   );
 }
 
